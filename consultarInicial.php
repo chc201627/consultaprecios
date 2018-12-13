d838a01ace934ad3729dac5dcc716d343f6dac80
camilo <chc201627@gmail.com>
2018-12-12 20:42:51 -0500

    modificando vista index

--- consultarInicial.php
+++ consultarInicial.php
@@ -20,10 +20,27 @@
 
 
         while ($fila = sqlsrv_fetch_array($stmt)) {
-            echo '<h1>'."Precios: ".utf8_encode($fila['f_simbolo'])."".number_format($fila['f_precio_vta'],0,",",".").'</h1>';
             echo "<br>";
-            echo '<h1>'.utf8_encode($fila['f_item_resumen']).'</h1>';
             echo "<br>";
+            echo "<br>";
+            echo "<br>";
+            echo "<div class='container'>";
+            echo "<div class='row'> ";
+            echo "<div class='col-xs-7 col-xs-offset-5'>";
+            echo '<h1>'.utf8_encode($fila['f_simbolo'])."".number_format($fila['f_precio_vta'],0,",",".").'</h1>';
+            echo "<br>";
+            echo "<br>";
+            echo "<br>";
+            echo "</div>";
+            echo "</div>";
+            echo "<div class='row'> ";
+            echo "<div class='col-xs-9 col-xs-offset-2'>";
+            $cadena = utf8_encode($fila['f_item_resumen']);
+            echo '<h1>'.preg_replace('/[0-9]{7}/', '', $cadena).'</h1>';
+            echo "<br>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
         }
 
         header('refresh:5; url=http://localhost/carousel/index.php');
@@ -32,5 +49,7 @@
 
 <html>
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -97,7 +97,7 @@ $active="active";
 
   <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 col-lg-offset-2 col-xs-offset-2">
       <form action="consultarInicial.php" method="post">
-          <input type="text" class="form-control text-consultar" name="codigo" placeholder="DIGITE EL CODIGO DE BARRAS" >
+          <input type="text" autofocus class="form-control text-consultar" name="codigo" placeholder="VERIFIQUE EL CODIGO DE BARRAS" >
           <input type="submit" name="consultar" id="" class="form-control btn btn-primary btn-consultar">
       </form>
   </div>
